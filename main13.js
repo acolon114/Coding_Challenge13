843198415a436d672145e74d408b331b3a369f0e
acolon114 <acolon114@usf.edu>
2024-11-03 23:18:43 +0000

    Format product information dynamically

--- main13.js
+++ main13.js
@@ -10,3 +10,18 @@ fetch('https://www.course-api.com/javascript-store-products')
 .then(data => {
     data.forEach(product => displayProduct(product));
 })
+
+// Task 3: Display Product Details Dynamically
+function displayProduct(product) {
+    const productElement = document.createElement('div');
+    productElement.classList.add('product');
+    
+    productElement.innerHTML = `
+        <img src="${product.fields.image[0].url}" alt="${product.fields.name}">
+        <h3>${product.fields.name}</h3>
+        <p>Company: ${product.fields.company}</p>
+        <p>Price: $${(product.fields.price / 100).toFixed(2)}</p>
+    `;
+    
+    container.appendChild(productElement);
+}
